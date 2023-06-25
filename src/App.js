d619f99d41f8e05b6b41fa5b1fea1b82e8094d71
pranavkotwal <pkotwal24@gmail.com>
2023-06-25 17:30:22 +0530

    added firebase

--- src/App.js
+++ src/App.js
@@ -2,50 +2,89 @@ import CartItem from "./CartItem";
 import React from "react";
 import Cart from "./Cart";
 import Navbar from "./Navbar";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+
+
 class App extends React.Component {
   
    constructor(){
         super()
         this.state ={
-            products:[
-                {
-                    price:929,
-                    title:'Apple watch',
-                    qty:1,
-                    img:'https://images.unsplash.com/photo-1525598912003-663126343e1f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
-                    id:1
-                },
-                {
-                    price:224,
-                    title:'Noise watch',
-                    qty:1,
-                    img:'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=776&q=80',
-                    id:2
-                },
-                {
-                    price:124,
-                    title:'Casio watch',
-                    qty:1,
-                    img:'https://images.unsplash.com/photo-1522312346375-d1a52e2b99b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=788&q=80',
-                    id:3
-                }
-            ]
-            
-
+            products:[],
+            loading:true
         }
-        
-
+        // Get the Firestore instance
+        this.db = firebase.firestore()
     }
+
+
+     componentDidMount() {
+    // const db = firebase.firestore(); // Get the Firestore instance
+    // db.collection('products') // Pass the collection name directly as a string
+    //   .get()
+    //   .then((snapshot) => {
+    //     // console.log(snapshot);
+    //     snapshot.docs.map((doc)=>{
+    //       console.log(doc.data())
+    //     })
+    //     const products = snapshot.docs.map((doc)=>{
+    //       const data=doc.data()
+    //       data['id'] = doc.id
+    //       return data
+    //     })
+    //     this.setState({
+    //       products:products,
+    //       loading:false
+    //     })
+    //   });
+
+
+
+    
+    this.db.collection('products') // Pass the collection name directly as a string
+    // .where('price','<',5100) 
+    .orderBy('price')
+    .onSnapshot((snapshot) => {
+        // console.log(snapshot);
+        snapshot.docs.map((doc)=>{
+          console.log(doc.data())
+        })
+        const products = snapshot.docs.map((doc)=>{
+          const data=doc.data()
+          data['id'] = doc.id
+          return data
+        })
+        this.setState({
+          products:products,
+          loading:false
+        })
+      })
+  }
+
+
+  
 
     handleIncreaseQuantity=(product)=>{
         console.log('Hey Please increase the quantity of ',product)
         const {products} = this.state
         const index = products.indexOf(product)
 
-        products[index].qty += 1 
-        this.setState({
-            products: products
-        })
+        // products[index].qty += 1 
+        // this.setState({
+        //     products: products
+        // })
+      const docRef = this.db.collection('products').doc(products[index].id)
+      docRef.update({
+        qty:products[index].qty + 1
+      })
+      .then(()=>{
+        console.log('document Updated sucessfuly')
+      }).catch((err)=>{
+        console.log('Error',err)
+      })
+
     }
     handleDeceraseQuantity=(product)=>{
         // console.log('Hey Please increase the quantity of ',product)
@@ -55,19 +94,40 @@ class App extends React.Component {
         if(products[index].qty == 1){
             return;
         }
-        products[index].qty -= 1
-           this.setState({
-            products: products
+        // products[index].qty -= 1
+        //    this.setState({
+        //     products: products
+        // })
+        const docRef = this.db.collection('products').doc(products[index].id)
+
+        docRef.update({
+          qty:products[index].qty-1
+        })
+        .then(()=>{
+          console.log("document updated sucessfully")
+        }).catch((err)=>{
+          console.log('Error',err)
         })
               
     }
 
     handleDeleteProduct = (id) =>{
         const {products} = this.state;
-        const items = products.filter((item)=> item.id !== id)
-        this.setState({
-            products:items
+        // const items = products.filter((item)=> item.id !== id)
+        // this.setState({
+        //     products:items
+        // })
+
+        const docRef = this.db.collection('products').doc(id)
+
+        docRef.delete()
+        .then(()=>{
+          console.log('Deleted Sucessfully')
+        }).catch((err)=>{
+          console.log('Error',err)
         })
+
+
     }
     getCartCount=()=> {
       const{products} = this.state
@@ -91,13 +151,32 @@ class App extends React.Component {
       return cartTotal
 
     }
+
+    addProduct = ()=>{
+      this.db
+      .collection('products')
+      .add({
+        img:'',
+        price:900,
+        qty:3,
+        title:'Washing Machine'
+      })
+      .then((docref)=>{
+        console.log('Product has been added',docref)
+
+      })
+      .catch((err)=>{
+        console.log('Error :',err)
+      })
+    }
   
   render(){
-    const {products} = this.state
+    const {products,loading} = this.state
   
   return (
     <div className="App">
       <Navbar count={this.getCartCount()} />
+      {/* <button onClick={this.addProduct} style={{padding:10,fontSize:20}}>Add a Product</button> */}
       
       <Cart
       products = {products}
@@ -109,6 +188,7 @@ class App extends React.Component {
       
       
       />
+      {loading && <h1>Loading Products...</h1>}
       <div style={{fontSize:20, padding:10}}>TOTAL : {this.getCartTotal()}</div>
 
      
--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom/client';
 import CartItem from './CartItem';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBO3Ajx0igOFvjznUv7R4TL2zs4x4_TFZ8",
+    authDomain: "cart-5a2b6.firebaseapp.com",
+    projectId: "cart-5a2b6",
+    storageBucket: "cart-5a2b6.appspot.com",
+    messagingSenderId: "819353597243",
+    appId: "1:819353597243:web:6ed99a9c16c70145a0f31e"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +26,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
